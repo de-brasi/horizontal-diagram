b59a39de3f37d2828681c604d3a17eda30a2e89e
de_brasi <ya.kakadu.ru@yandex.ru>
2022-11-21 20:34:24 +0300

    Minimal working shitty version

--- app/src/main/java/health/helper/posturecorrectorstatisticviewer/view/HorizontalDiagram.java
+++ app/src/main/java/health/helper/posturecorrectorstatisticviewer/view/HorizontalDiagram.java
@@ -18,7 +18,7 @@ public class HorizontalDiagram extends View {
     // ---Private members---\
     private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Map<Integer, Float> graph_data =
-            Map.of(1, 2F, 2, 3F, 4, 4F);
+            Map.of(1, 1F, 2, 2F, 3, 3F);
     // ---Private members---/
 
     public HorizontalDiagram(Context context) {
@@ -39,8 +39,10 @@ public class HorizontalDiagram extends View {
         super.onDraw(canvas);
 
         final float asxisStrokeWidth = 0.07F;
-        float axisX_offset = 0.5F;  // отступ начала координат в нормальной системе координат
-        float axisY_offset = -2F;   // отступ начала координат в нормальной системе координат
+        // отступ начала координат в нормальной системе координат от края холста
+        float axisX_offset = 0.5F;
+        // отступ начала координат в нормальной системе координат от края холста
+        float axisY_offset = -2F;
 
         paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.GRAY);
@@ -55,22 +57,30 @@ public class HorizontalDiagram extends View {
         float yMax = 10F;   // axis Y size
         float yMin = 0F;
 
+        float xEndingOffset = 1F;
+        float yEndingOffset = 1F;
+
         float width = getWidth();
         float height = getHeight();
         float scale_parameter = Math.min(width, height); // what size chosen as standard
 
         changeAxis(canvas, scale_parameter, xMin, xMax, yMin, yMax, axisX_offset, axisY_offset);
 
+        canvas.drawCircle(0, 0, 0.25F, paint);
         // axis X and Y
         printWithScale_AxisX(
                 canvas, xMin, xMax, yMin, yMax,
-                1F, asxisStrokeWidth, 4);
+                xEndingOffset, asxisStrokeWidth, 3);
         printWithScale_AxisY(
                 canvas, xMin, xMax, yMin, yMax,
-                1F, asxisStrokeWidth, 4);
+                yEndingOffset, asxisStrokeWidth, 3);
 
         // Graph data printing
-        printDiagramData(canvas, graph_data, xMax, xMin, yMax, yMin);
+        printDiagramData(
+                canvas, graph_data,
+                0F, 0F,
+                xMax - xMin - xEndingOffset, yMax - yMin - yEndingOffset,
+                graph_data.size());
 
         // ---Diagram printing end---
 
@@ -129,8 +139,56 @@ public class HorizontalDiagram extends View {
     }
 
     protected void printDiagramData(Canvas canvas, Map<Integer, Float> graph_data,
-                                    float xMax, float xMin,
-                                    float yMax, float yMin) {
+                                    float coordinateAxisZeroDot_x, float coordinateAxisZeroDot_y,
+                                    float axisX_length, float axisY_length,
+                                    int scaleCount_y) {
+        // Исходя из личных предпочтений о "гармонии и красоте"
+        // выбирается значение workspace_share (0 < workspace_share <= (axsiMax - axisMin)) -
+        // это значение обозначает долю области от общего пространства,
+        // которую может занимать горизонтальная полоска.
+        // Максимальное значение в данных диаграмы заполняет workspace_share на 100%,
+        // остальные значения длины полоски при отрисовке из данных пересчитываются
+        // относительно макимума по формуле: (val / max_val) * workspace_share
 
+        float workspace_share = 0.8F;
+        float workspace_len = axisX_length * workspace_share;
+
+        float distanceBetweenScales_y = axisY_length / (scaleCount_y + 1);
+
+        int curScale;
+        float curValue;
+        float curLength;
+        float distanceFromAxisOX;
+        paint.setColor(Color.RED);
+
+        float maxValue = getMaximumValueFromMap(graph_data);
+
+        for (Map.Entry<Integer, Float> entry: graph_data.entrySet()) {
+            curScale = entry.getKey();
+            curValue = entry.getValue();
+
+            curLength = (curValue / maxValue) * workspace_len;
+            distanceFromAxisOX = distanceBetweenScales_y * curScale;
+
+            canvas.drawLine(
+                    coordinateAxisZeroDot_x,
+                    coordinateAxisZeroDot_y + distanceFromAxisOX,
+                    coordinateAxisZeroDot_x + curLength,
+                    coordinateAxisZeroDot_y + distanceFromAxisOX,
+                    paint
+            );
+        }
+    }
+
+    protected  <K, V extends Comparable<V>> V getMaximumValueFromMap(Map<K, V> targetMap) {
+        V maxValue = null;
+
+        for (Map.Entry<K, V> entry: targetMap.entrySet()) {
+            if (maxValue == null || entry.getValue().compareTo(maxValue) > 0) {
+                maxValue = entry.getValue();
+            }
+        }
+
+        return maxValue;
     }
 }
